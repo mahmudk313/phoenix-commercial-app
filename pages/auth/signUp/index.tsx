65f054e91594a532023d8a576df741e130c36311
MahmudK <mahmudkenway@gmail.com>
2022-09-15 11:47:28 +0430

    Create signUp page

---
+++ pages/auth/signUp/index.tsx
@@ -0,0 +1,10 @@
+import { NextPage } from "next";
+import Register from "../../../app/components/auth/register";
+
+const SignUp: NextPage = () => {
+    return(
+        <Register />
+    )
+}
+
+export default SignUp;
