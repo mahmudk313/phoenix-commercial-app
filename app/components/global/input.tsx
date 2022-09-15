05edd3da924ccfdf70edb827c18dba5d3b073406
MahmudK <mahmudkenway@gmail.com>
2022-09-15 12:41:04 +0430

    Update Input comp

--- app/components/global/input.tsx
+++ app/components/global/input.tsx
@@ -1,29 +1,34 @@
 import { Field } from "formik";
+import { ErrorMessage } from "formik";
 import React from "react";
 
 interface Props{
     name : string,
     label : string,
-    className?: string,
-    type?: string,
+    type? : string,
+    className? : string,
+    inputClassName? : string,
+    labelClassName? : string,
+    errorClassName? : string,
 }
 
-const Input : React.FC<Props> = ({ name, label, className, type }) => {
+const Input : React.FC<Props> = ({ name, label, className, type = "text", errorClassName, labelClassName, inputClassName }) => {
 
 
     return(
         <>
-            <div className={className ? className : "col-span-6 sm:col-span-3"}>
-                <label htmlFor={name} className="block text-sm font-medium text-gray-700">
+            <div className={`col-span-6 sm:col-span-3 ${className ?? ''}`}>
+                <label htmlFor={name} className={`block text-sm font-medium text-gray-700 ${labelClassName ?? ''}`}>
                     {label}
                 </label>
                 <Field
-                    type={type? type : "text"}
+                    type={type}
                     name={name}
                     id={name}
                     autoComplete={name}
-                    className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                    className={`mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm ${inputClassName ?? ''}`}
                 />
+                <ErrorMessage name={name} className={`text-red-500 text-sm ${errorClassName ?? ''}`} component="div" />
             </div>
         </>
     )
