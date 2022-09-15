bb6a1216c3d380afd11fcd4bb2be121a85d96792
MahmudK <mahmudkenway@gmail.com>
2022-09-15 11:47:42 +0430

    Create registerForm comp

--- app/components/auth/register/index.tsx
+++ app/components/auth/register/index.tsx
@@ -1,35 +1,14 @@
 import React from 'react'
-import Input from '../../global/input'
+import RegisterForm from './registerForm';
 
 const Register:React.FC = () => {
-  return(
-    <div className="h-screen sm:mt-0 flex justify-center">
-      <div className="">
-        <div className="mt-9 md:col-span-2">
-          <form action="#" method="POST">
-            <div className="overflow-hidden shadow sm:rounded-md">
-              <div className="bg-white px-4 py-5 sm:p-6">
-                <div className="grid grid-cols-6 gap-6">
-                  
-                  <Input name="first-name" label="First Name" />
-                  <Input name="last-name" label="Last name" />
-                  <Input name="email" label="Email" className="col-span-6 sm:col-span-6" />
-                  <Input name="Password" label="Password" className="col-span-6 sm:col-span-3" type="password" />
-                  <Input name="conf-Password" label="Confirm Password" className="col-span-6 sm:col-span-3" type="password" />
 
-                </div>
-              </div>
-              <div className="bg-gray-50 px-4 py-3 text-right sm:px-6">
-                <button
-                  type="submit"
-                  className="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                >
-                  Submit
-                </button>
-              </div>
-            </div>
-          </form>
-        </div>
+  return(
+    <div className="h-screen flex justify-center items-center bg-violet-200">
+      <div className="md:col-span-2">
+        
+        <RegisterForm />
+
       </div>
     </div>
   )
