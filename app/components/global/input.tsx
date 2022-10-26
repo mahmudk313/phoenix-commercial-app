56a9810aa1ea40c20a8950ba94d4a9dc556fe47c
MahmudK <mahmudkenway@gmail.com>
2022-10-26 12:06:23 +0330

    Update Input comp
    - add placeholder
    - make label optional
    - modify Filed className

--- app/components/global/input.tsx
+++ app/components/global/input.tsx
@@ -1,32 +1,38 @@
-import { Field } from "formik";
-import { ErrorMessage } from "formik";
+import { Field, ErrorMessage } from "formik";
 import React from "react";
 
 interface Props{
     name : string,
-    label : string,
+    label? : string,
     type? : string,
+    placeholder? : string,
     className? : string,
     inputClassName? : string,
     labelClassName? : string,
     errorClassName? : string,
 }
 
-const Input : React.FC<Props> = ({ name, label, className, type = "text", errorClassName, labelClassName, inputClassName }) => {
+const Input : React.FC<Props> = ({ name, label, className, placeholder, type = "text", errorClassName, labelClassName, inputClassName }) => {
 
 
     return(
         <>
             <div className={`col-span-6 sm:col-span-3 ${className ?? ''}`}>
-                <label htmlFor={name} className={`block text-sm font-medium text-gray-700 ${labelClassName ?? ''}`}>
-                    {label}
-                </label>
+                {
+                    label
+                        ? <label htmlFor={name} className={`block text-sm font-medium text-gray-700 ${labelClassName ?? ''}`}>
+                            {label}
+                        </label>
+                        : ''
+                }
+                
                 <Field
                     type={type}
                     name={name}
                     id={name}
+                    placeholder={placeholder}
                     autoComplete={name}
-                    className={`mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm ${inputClassName ?? ''}`}
+                    className={`${label ? "mt-1" : ''} block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm ${inputClassName ?? ''}`}
                 />
                 <ErrorMessage name={name} className={`text-red-500 text-sm ${errorClassName ?? ''}`} component="div" />
             </div>
