ee46d2876e60581cb878fb6f4cbd2e91ea1d88da
MahmudK <mahmudkenway@gmail.com>
2022-10-26 11:04:22 +0330

    Create signIn page

---
+++ pages/auth/signIn/index.tsx
@@ -0,0 +1,14 @@
+import { NextPage } from "next";
+import LogIn from "../../../app/components/auth/login";
+
+
+
+const SignIn : NextPage = () => {
+    return(
+        <>
+            <LogIn />
+        </>
+    )
+}
+
+export default SignIn;
