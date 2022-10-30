a127904744b75b37a5ec7a8ca374a1e13a0eb9c5
MahmudK <mahmudkenway@gmail.com>
2022-10-30 10:48:31 +0330

    Refactor loginForm

--- app/forms/auth/loginForm.tsx
+++ app/forms/auth/loginForm.tsx
@@ -1,79 +1,14 @@
 import React from "react";
-import { LockClosedIcon } from '@heroicons/react/20/solid'
 import { Form, FormikProps, withFormik } from "formik";
 
 //Components
-import Input from "../../components/global/input";
-
-interface LoginFormValues {
-    email: string,
-    password: string
-}
-
-
-const InnerLoginForm = (props : FormikProps<LoginFormValues>) => {
-    return (
-        <Form className="mt-8 space-y-6" action="#" method="POST">
-            <input type="hidden" name="remember" defaultValue="true" />
-            <div className="-space-y-px rounded-md shadow-sm">
-                <div>
-                    <Input
-                        name="email"
-                        type="email"
-                        inputClassName="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                        placeholder="Email address"
-                    />
-                </div>
-                <div>
-                    <Input
-                        name="password"
-                        type="password"
-                        inputClassName="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                        placeholder="Password"
-                    />
-                </div>
-            </div>
-
-            <div className="flex items-center justify-between">
-                <div className="flex items-center">
-                    <input
-                        id="remember-me"
-                        name="remember-me"
-                        type="checkbox"
-                        className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
-                    />
-                    <label htmlFor="remember-me" className="ml-2 block text-sm text-gray-900">
-                        Remember me
-                    </label>
-                </div>
-
-                <div className="text-sm">
-                    <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
-                        Forgot your password?
-                    </a>
-                </div>
-            </div>
-
-            <div>
-                <button
-                    type="submit"
-                    className="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                >
-                    <span className="absolute inset-y-0 left-0 flex items-center pl-3">
-                        <LockClosedIcon className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" aria-hidden="true" />
-                    </span>
-                    Sign in
-                </button>
-            </div>
-        </Form>
-    )
-}
+import { LoginFormValuesInterface } from "../../contracts/auth";
+import InnerLoginForm from "../../components/auth/innerLoginForm";
 
 interface loginFormProps {
-
 }
 
-const LoginForm = withFormik<loginFormProps, LoginFormValues>({
+const LoginForm = withFormik<loginFormProps, LoginFormValuesInterface>({
     mapPropsToValues : props => {
         return {
             email : "",
