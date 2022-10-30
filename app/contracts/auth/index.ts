c93b6af07827c481a1c3c66b46004aa7696ae552
MahmudK <mahmudkenway@gmail.com>
2022-10-30 10:53:46 +0330

    Refactor registerForm

--- app/contracts/auth/index.ts
+++ app/contracts/auth/index.ts
@@ -2,3 +2,10 @@ export interface LoginFormValuesInterface {
     email: string,
     password: string
 }
+
+export interface RegisterFormValuesInterface {
+    name : string,
+    lastName : string,
+    email : string,
+    password : string
+}
--- app/forms/auth/registerForm.tsx
+++ app/forms/auth/registerForm.tsx
@@ -1,46 +1,15 @@
-import { Form, FormikProps, withFormik } from 'formik'
-import Input from '../../components/global/input'
+import { withFormik } from 'formik'
+
+
+import InnerRegisterForm from '../../components/auth/innerRegisterForm'
+import { RegisterFormValuesInterface } from '../../contracts/auth'
 import registerSchema from '../../schema/registerSchema'
-
-interface RegisterFormValues {
-    name : string,
-    lastName : string,
-    email : string,
-    password : string
-}
-
-const InnerRegisterForm = (props : FormikProps<RegisterFormValues>) => {
-    return (
-        <Form action="#" method="POST">
-            <div className="overflow-hidden shadow sm:rounded-md">
-                <div className="bg-white px-4 py-5 sm:p-6">
-                    <div className="grid grid-cols-6 gap-6">
-
-                        <Input name="name" label="First Name" />
-                        <Input name="lastName" label="Last name" />
-                        <Input name="email" label="Email" type="email" className="col-span-6 sm:col-span-6" />
-                        <Input name="password" label="Password" className="col-span-6 sm:col-span-6" type="password" />
-
-                    </div>
-                </div>
-                <div className="bg-gray-100 px-4 py-3 text-right sm:px-6">
-                    <button
-                        type="submit"
-                        className="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                    >
-                        Submit
-                    </button>
-                </div>
-            </div>
-        </Form>
-    )
-}
 
 interface RegisterFormProps {
 
 }
 
-const RegisterForm = withFormik<RegisterFormProps, RegisterFormValues>({
+const RegisterForm = withFormik<RegisterFormProps, RegisterFormValuesInterface>({
     mapPropsToValues : props => {
         return {
             name : "",
