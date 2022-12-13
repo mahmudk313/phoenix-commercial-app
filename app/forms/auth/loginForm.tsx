125896597668697a65d861ba8ab32fa5ae2027b9
MahmudK <mahmudkenway@gmail.com>
2022-12-13 09:17:59 +0330

    Error handling on login submit

--- app/forms/auth/loginForm.tsx
+++ app/forms/auth/loginForm.tsx
@@ -5,6 +5,7 @@ import { Form, FormikProps, withFormik } from "formik";
 import { LoginFormValuesInterface } from "../../contracts/auth";
 import InnerLoginForm from "../../components/auth/innerLoginForm";
 import callApi from "../../helpers/callApi";
+import ValidationError from "../../exceptions/validationError";
 
 interface loginFormProps {
     setCookie : any,
@@ -17,15 +18,22 @@ const LoginForm = withFormik<loginFormProps, LoginFormValuesInterface>({
             password : ""
         }
     },
-    handleSubmit : async (values, { props }) => {
-        const res = await callApi().post('auth/login', values)
-        if (res.status === 200) {
-            props.setCookie('phoenix-token', res.data.token, {
-                'maxAge' : 3600 * 24 * 30,
-                'sameSite'  : 'lax',
-                'domain' : 'localhost',
-                'path' : '/'
-            })
+    handleSubmit : async (values, { props , setFieldError }) => {
+        try {
+            const res = await callApi().post('auth/login', values)
+            if (res.status === 200) {
+                props.setCookie('phoenix-token', res.data.token, {
+                    'maxAge' : 3600 * 24 * 30,
+                    'sameSite'  : 'lax',
+                    'domain' : 'localhost',
+                    'path' : '/'
+                })
+            }
+        }
+        catch (error) {
+            if(error instanceof ValidationError) {
+                Object.entries(error.messages).forEach( ( [key , value]) => setFieldError (key, value as string))
+            }
         }
     }
 })(InnerLoginForm)
