bf06e51714e2dc0dfd0200e35faee1c6e0ea20ea
MahmudK <mahmudkenway@gmail.com>
2023-06-22 14:47:04 +0330

    Update innerLoginForm.tsx
    - add phone input

--- app/components/auth/innerLoginForm.tsx
+++ app/components/auth/innerLoginForm.tsx
@@ -12,34 +12,14 @@ const InnerLoginForm = (props : FormikProps<LoginFormValuesInterface>) => {
             <div className="-space-y-px rounded-md shadow-sm">
                 <div>
                     <Input
-                        name="email"
-                        type="email"
-                        inputClassName="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                        placeholder="Email address"
-                    />
-                </div>
-                <div>
-                    <Input
-                        name="password"
-                        type="password"
-                        inputClassName="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                        placeholder="Password"
+                        name="phone"
+                        inputClassName="relative block w-full appearance-none border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                        placeholder="Phone number"
                     />
                 </div>
             </div>
 
             <div className="flex items-center justify-between">
-                <div className="flex items-center">
-                    <input
-                        id="remember-me"
-                        name="remember-me"
-                        type="checkbox"
-                        className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
-                    />
-                    <label htmlFor="remember-me" className="ml-2 block text-sm text-gray-900">
-                        Remember me
-                    </label>
-                </div>
 
                 <div className="text-sm">
                     <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
