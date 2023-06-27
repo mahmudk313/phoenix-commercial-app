f7a629de522616588ae4521a09aac01caead7ce8
MahmudK <mahmudkenway@gmail.com>
2023-06-27 11:30:08 +0330

    Define handleSubmit in verifyPhoneForm

--- app/forms/auth/verifyPhoneForm.tsx
+++ app/forms/auth/verifyPhoneForm.tsx
@@ -1,8 +1,12 @@
 import { withFormik } from "formik";
 
+//helpers
+import callApi from "../../helpers/callApi";
 
 //Components
 import { VerifyPhoneFormValuesInterface } from "../../contracts/auth";
+import Router from "next/router";
+import ValidationError from "../../exceptions/validationError";
 import innerVerifyPhoneForm from "../../components/auth/innerVerifyPhoneForm";
 
 interface verifyPhoneFormProps {
@@ -17,6 +21,25 @@ const VerifyPhoneForm = withFormik<verifyPhoneFormProps, VerifyPhoneFormValuesIn
             code : 0
         }
     },
+    handleSubmit : async (values, { props , setFieldError } ) => {
+        try {
+            const res = await callApi().post('auth/verify-phone', values)
+            if (res.status === 200) {
+                props.setCookies("phoenix-token", res.data.token, {
+                    'max-age' : 3600 * 24 * 30,
+                    'sameSite' : 'lax',
+                    'domain' : 'localhost',
+                    'path' : '/'
+                })
+
+                Router.push('/dashboard')
+            }
+        } catch (error) {
+            if (error instanceof ValidationError) {
+                Object.entries(error.messages).forEach(([key, value]) => setFieldError(key , value as string))
+            }
+        }
+    }
 })(innerVerifyPhoneForm)
 
 export default VerifyPhoneForm;
