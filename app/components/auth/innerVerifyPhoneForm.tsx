0135f498ee3d03989487bab9eca2a61a6c474dc3
MahmudK <mahmudkenway@gmail.com>
2023-06-27 11:54:36 +0330

    Create innerVerifyPhoneForm.tsx

---
+++ app/components/auth/innerVerifyPhoneForm.tsx
@@ -0,0 +1,10 @@
+import { Form, FormikProps } from "formik";
+import { VerifyPhoneFormValuesInterface } from "../../contracts/auth";
+
+
+const InnerVerifyPhoneForm = (props : FormikProps<VerifyPhoneFormValuesInterface>) => {
+    return (
+    )
+}
+
+export default InnerVerifyPhoneForm;
