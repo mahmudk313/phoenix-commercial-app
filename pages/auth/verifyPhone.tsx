6a28a5cd10ed3a5c15e47bdf2617dfe5a31c0b9a
MahmudK <mahmudkenway@gmail.com>
2023-06-27 10:45:36 +0330

    Update verifyPhone.tsx

--- pages/auth/verifyPhone.tsx
+++ pages/auth/verifyPhone.tsx
@@ -6,6 +6,25 @@ const VerifyPhone : NextPage = () => {
     const [cookies, setCookie] = useCookies(['phoenix-token'])
 
     return(
+        <>
+            <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+                <div className="w-full max-w-md space-y-8">
+                    <div>
+                        <img
+                        className="mx-auto h-12 w-auto"
+                        src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                        alt="Your Company"
+                        />
+                        <h2 className="mt-6 text-center text-2xl font-bold tracking-tight text-gray-900">
+                            Enter the code sent to your phone
+                        </h2>
+                        
+                    </div>
+
+                    
+                </div>
+            </div>
+        </>
     )
 }
 
